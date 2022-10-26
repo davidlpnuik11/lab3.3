22f27281aa99365d45a0f332c21c72501452c043
davidlpnuik11 <davidzxcneghoul@gmail>
2022-10-26 19:51:33 +0300

    lab3.3

---
+++ PR3.3/PR3.3.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+		double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -4)
+		y = -2;
+	else
+		if (-4 < x && x <= 0)
+			y = (-5 / 2) * x - 10;
+		else
+			if (0 < x && x <= 2)
+				y = x * x;
+			else
+				y = -2 * x + 4;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
